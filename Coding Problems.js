9f0796fd888063da8f725e54d90a1f3ec4463cd0
Subhash Kandhway <kandhway002subhash@gmail.com>
2023-04-18 17:34:08 +0530

    Update Coding Problems.js

--- Coding Problems.js
+++ Coding Problems.js
@@ -379,3 +379,20 @@ async function sleep(millis) {
  * let t = Date.now()
  * sleep(100).then(() => console.log(Date.now() - t)) // 100
  */
+
+2619. Array Prototype Last
+Easy
+93
+4
+Companies
+Write code that enhances all arrays such that you can call the array.last() method on any array and it will return the last element. If there are no elements in the array, it should return -1.
+ Array.prototype.last = function() {
+   if(this.length===0){
+       return -1;
+   } else return this[this.length-1]
+};
+
+/**
+ * const arr = [1, 2, 3];
+ * arr.last(); // 3
+ */
