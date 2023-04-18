da981a0966d4a853d50b10f5754c864edd157b1e
Subhash Kandhway <kandhway002subhash@gmail.com>
2023-04-18 14:38:20 +0530

    Update Coding Problems.js

--- Coding Problems.js
+++ Coding Problems.js
@@ -320,3 +320,28 @@ Other Methods-
     }
     return results.flat().join('');
 }
+
+2620. Counter
+Easy
+116
+3
+Companies
+Given an integer n, return a counter function. This counter function initially returns n and then returns 1 more than the previous value every subsequent time it is called (n, n + 1, n + 2, etc).
+/**
+ * @param {number} n
+ * @return {Function} counter
+ */
+var createCounter = function(n) {
+    let x=n
+    return function() {
+         x+=1;
+        return x-1;
+    };
+};
+
+/** 
+ * const counter = createCounter(10)
+ * counter() // 10
+ * counter() // 11
+ * counter() // 12
+ */
